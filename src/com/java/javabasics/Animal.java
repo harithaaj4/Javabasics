e1177eb1901ce642b498b97115ca1e7e34f7d05b
Haritha Jami <harithajami2004@gmail.com>
2025-08-25 09:49:59 +0530

    code commit

---
+++ src/com/java/javabasics/Animal.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.java.javabasics;
+
+/**
+ * @author harit
+ *
+ */
+public class Animal {
+
+	/**
+	 * @param args
+	 */
+	
+		// TODO Auto-generated method stub
+ void eat() {
+	System.out.println("eating");
+}
+	}
+
